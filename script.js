35f042b02824bf0f930ea1956b348465551da23c
kinga j <kingajaglo.99@gmail.com>
2021-02-02 23:49:39 +0100

    feat: create side methods in Matrix class

--- script.js
+++ script.js
@@ -10,15 +10,34 @@ class Matrix {
         for (let row = 0; row < this.sizeX; row++) {
             const matrixRow = [];
             for (let col = 0; col < this.sizeY; col++) {
-                console.log(this.sizeX, this.sizeY);
                 matrixRow.push(true);
-                console.log(this.matrixRow);
             }
-            this.matrix.push(this.matrixRow);
+            this.matrix.push(matrixRow);
         }
         console.log(this.matrix);
     }
+
+    sideLeft(col) {
+        if (col == 0) {
+            console.log("left");
+        }
+    }
+
+    sideRight(col) {
+        if (col == 9) {
+            console.log("right");
+        }
+    }
+
+    sideBottom(row) {
+        if (row == 19) {
+            console.log("bottom");
+        }
+    }
 }
 
-const matrixMain = new Matrix(10, 20);
+const matrixMain = new Matrix(20, 10);
 matrixMain.draw();
+matrixMain.sideLeft(0)
+matrixMain.sideRight(0)
+matrixMain.sideBottom(0)
