ee6b93925dae0894bbaa969463163a9555fdc2b0
kinga j <kingajaglo.99@gmail.com>
2021-02-06 22:53:01 +0100

    feat: create move piece functions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+
 class Matrix {
 
     constructor(sizeX, sizeY) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.matrix = [];
+        this.matrix01 = [];
     }
 
     draw() {
@@ -15,6 +18,17 @@ class Matrix {
             this.matrix.push(matrixRow);
         }
         console.log(this.matrix);
+    }
+    draw01() {
+        for (let row = 0; row <
+            this.sizeX; row++) {
+            const matrixRow = [];
+            for (let col = 0; col < this.sizeY; col++) {
+                matrixRow.push(0);
+            }
+            this.matrix01.push(matrixRow);
+        }
+        console.log(this.matrix01);
     }
 
     sideLeft(col) {
@@ -38,9 +52,44 @@ class Matrix {
 
 const matrixMain = new Matrix(20, 10);
 matrixMain.draw();
+matrixMain.draw01();
+
 matrixMain.sideLeft(0)
 matrixMain.sideRight(0)
 matrixMain.sideBottom(0)
+
+class Piece {
+    constructor() {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.area = new Matrix(4, 4);
+        this.type = Math.round(Math.random() * 10) % 7;
+    }
+
+    moveRight() {
+        this.positionX = this.positionX + 1;
+        console.log("move Right")
+    }
+
+    moveLeft() {
+        this.positionX = this.positionX - 1;
+        console.log("move Left")
+    }
+
+    moveUp() {
+        this.positionY = this.positionY - 1;
+        console.log("move Up")
+    }
+
+    moveDown() {
+        this.positionY = this.positionY + 1;
+        console.log("move Down")
+    }
+
+}
+
+const pieceCurrent = new Piece();
+
 
 
 document.addEventListener("keydown", function (event) {
@@ -48,15 +97,22 @@ document.addEventListener("keydown", function (event) {
     switch (event.key) {
         case "ArrowUp":
             console.log("Up")
+            pieceCurrent.moveUp();
             break;
         case "ArrowDown":
             console.log("Down")
+            pieceCurrent.moveDown();
+
             break;
         case "ArrowLeft":
             console.log("Left")
+            pieceCurrent.moveLeft();
+
             break;
         case "ArrowRight":
             console.log("Right")
+            pieceCurrent.moveRight();
+
             break;
         case " ":
             console.log("Space")
@@ -72,3 +128,6 @@ playOnClick = () => {
 
     console.log("pplay")
 }
+
+
+
